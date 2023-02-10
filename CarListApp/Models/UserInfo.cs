cb259e885e6cb4465e7ad5e104c99179cf354461
Oleksandr Barylo <sbarylo20023@gmail.com>
2023-02-10 21:59:45 +0200

    Some refactoring

--- CarListApp/Models/UserInfo.cs
+++ CarListApp/Models/UserInfo.cs
@@ -2,7 +2,7 @@
 {
     public class UserInfo
     {
-        public string UserName { get; set; }
+        public string Username { get; set; }
         public List<string> Roles { get; set; }
     }
 }
